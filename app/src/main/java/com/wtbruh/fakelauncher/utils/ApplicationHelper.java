bf6ea77a6827557cd8f2de7c1984c25c34477484
ZH-XiJun <59949135+ZH-XiJun@users.noreply.github.com>
2025-04-18 13:53:37 +0800

    add ApplicationHelper to get top Activity

---
+++ app/src/main/java/com/wtbruh/fakelauncher/utils/ApplicationHelper.java
@@ -0,0 +1,56 @@
+package com.wtbruh.fakelauncher.utils;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ApplicationHelper extends Application {
+
+    public static String topActivity;
+    private final static String TAG = ApplicationHelper.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+                topActivity = activity.toString();
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+                topActivity = "";
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+
+            }
+        });
+    }
+}

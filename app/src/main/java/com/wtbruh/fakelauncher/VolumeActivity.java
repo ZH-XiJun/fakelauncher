8d8e9187e56fc8db29c3b3e310edbf51df087338
ZH-XiJun <59949135+ZH-XiJun@users.noreply.github.com>
2025-04-18 13:54:43 +0800

    add VolumeActivity to replace system's default volume panel

---
+++ app/src/main/java/com/wtbruh/fakelauncher/VolumeActivity.java
@@ -0,0 +1,35 @@
+package com.wtbruh.fakelauncher;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.wtbruh.fakelauncher.utils.MyAppCompatActivity;
+
+public class VolumeActivity extends MyAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_volume);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void getExtra(){
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("key");
+        if (data == null) {
+            return;
+        }
+
+    }
+}

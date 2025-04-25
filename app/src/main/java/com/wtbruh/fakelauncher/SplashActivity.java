bba5de5a03f1d6a3f57dad509cf245517ed91adf
ZH-XiJun <59949135+ZH-XiJun@users.noreply.github.com>
2025-04-25 13:27:40 +0800

    if starting main ui in app settings, user will able to back to settings ui with screen pinning stopped unexpectedly. add SplashActivity to fix the bug.

---
+++ app/src/main/java/com/wtbruh/fakelauncher/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.wtbruh.fakelauncher;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+}

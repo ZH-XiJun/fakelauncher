f5e1cbf839ec61b1856f8fbe7d5a7f1e8c211970
wt_bruhhhh <59949135+ZH-XiJun@users.noreply.github.com>
2025-05-25 17:14:07 +0800

    add a vibrate action on starting app to tell user app is started

--- app/src/main/java/com/wtbruh/fakelauncher/SplashActivity.java
+++ app/src/main/java/com/wtbruh/fakelauncher/SplashActivity.java
@@ -1,12 +1,12 @@
 package com.wtbruh.fakelauncher;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.wtbruh.fakelauncher.utils.UIHelper;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
@@ -14,9 +14,14 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Vibrate on launching fake ui
+        // todo: add preference to control vibration
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(200);
         startActivity(new Intent()
                 .setClass(SplashActivity.this, MainActivity.class)
                 .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
         finish();
     }
+
 }

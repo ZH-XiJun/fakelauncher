941b78f780c24807229ea8f9e35000617548e698
ZH-XiJun <59949135+ZH-XiJun@users.noreply.github.com>
2025-07-03 14:06:40 +0800

    refactor(feature phone UI): stroke text view

--- app/src/main/java/com/wtbruh/fakelauncher/utils/StrokeTextView.java
+++ app/src/main/java/com/wtbruh/fakelauncher/utils/StrokeTextView.java
@@ -1,74 +1,72 @@
 package com.wtbruh.fakelauncher.utils;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.wtbruh.fakelauncher.R;
+
 public class StrokeTextView extends androidx.appcompat.widget.AppCompatTextView {
-    private TextView outlineTextView = null;
+    private static final int[] STROKE_ATTRS = new int[]{R.attr.strokeTextColor, R.attr.strokeTextWidth};
+    private @ColorInt int strokeColor = 0;
+    private int strokeWidth = 3;
 
     public StrokeTextView(Context context) {
-        super(context);
-
-        outlineTextView = new TextView(context);
-        init();
+        this(context, null);
     }
 
-    public StrokeTextView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-
-        outlineTextView = new TextView(context, attrs);
-        init();
+    public StrokeTextView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, android.R.attr.textViewStyle);
     }
 
-    public StrokeTextView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-
-        outlineTextView = new TextView(context, attrs, defStyle);
-        init();
+    public StrokeTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, STROKE_ATTRS, defStyleAttr, 0);
+        strokeColor = typedArray.getColor(0, 0);
+        strokeWidth = typedArray.getInt(1, 3);
+        typedArray.recycle();
     }
 
-    public void init() {
-        TextPaint paint = outlineTextView.getPaint();
-        paint.setStrokeWidth(3);  //描边宽度
-        paint.setStyle(Paint.Style.STROKE);
-        outlineTextView.setTextColor(Color.parseColor("#000000"));  //描边颜色
-        outlineTextView.setGravity(getGravity());
+    public void setStrokeColor(@ColorInt int color) {
+        strokeColor = color;
+        invalidate();
     }
 
-    @Override
-    public void setLayoutParams (ViewGroup.LayoutParams params) {
-        super.setLayoutParams(params);
-        outlineTextView.setLayoutParams(params);
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-        //设置轮廓文字
-        CharSequence outlineText = outlineTextView.getText();
-        if (outlineText == null || !outlineText.equals(this.getText())) {
-            outlineTextView.setText(getText());
-            postInvalidate();
-        }
-        outlineTextView.measure(widthMeasureSpec, heightMeasureSpec);
-    }
-
-    @Override
-    protected void onLayout (boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-        outlineTextView.layout(left, top, right, bottom);
+    public void setStrokeWidth(int width) {
+        strokeWidth = width;
+        invalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        outlineTextView.draw(canvas);
+        TextPaint wkPaint = getLayout().getPaint();
+        int preColor = wkPaint.getColor();
+        Paint.Style prePaintStyle = wkPaint.getStyle();
+        // apply stroke paint
+        wkPaint.setColor(strokeColor);
+        wkPaint.setStrokeWidth(strokeWidth);
+        wkPaint.setStyle(Paint.Style.STROKE);
+        // draw text outline
+        getLayout().draw(canvas);
+
+        // restore paint
+        wkPaint.setColor(preColor);
+        wkPaint.setStrokeWidth(0);
+        wkPaint.setStyle(prePaintStyle);
         super.onDraw(canvas);
     }
+
+
 }

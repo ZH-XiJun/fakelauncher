676a642b442ea2eb3bcbaa4101478190af016745
ZH-XiJun <59949135+ZH-XiJun@users.noreply.github.com>
2025-12-25 14:01:26 +0800

    feat: add attributes for FitTextView

--- app/src/main/java/com/wtbruh/fakelauncher/ui/widget/FitTextView.java
+++ app/src/main/java/com/wtbruh/fakelauncher/ui/widget/FitTextView.java
@@ -1,6 +1,7 @@
 package com.wtbruh.fakelauncher.ui.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 
@@ -8,7 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.widget.TextViewCompat;
 
+import com.wtbruh.fakelauncher.R;
+
 public class FitTextView extends androidx.appcompat.widget.AppCompatTextView {
+
+    private final static int[] FIT_ATTRS = new int[]{R.attr.fitWidth, R.attr.fitHeight};
 
     public FitTextView(@NonNull Context context) {
         super(context);
@@ -20,6 +25,11 @@ public class FitTextView extends androidx.appcompat.widget.AppCompatTextView {
 
     public FitTextView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray tr = context.obtainStyledAttributes(attrs, FIT_ATTRS, defStyleAttr, 0);
+        if (tr.getBoolean(0, false)) fitWidth();
+        if (tr.getBoolean(1, false)) fitHeight();
+        tr.recycle();
+
     }
 
     public void fitWidth() {
